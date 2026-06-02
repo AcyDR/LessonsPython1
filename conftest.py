3355abe0ce85917e9e86e288db96a1889635df71
Anton <dcnastya@gmail.com>
2026-06-02 19:57:59 +0300

    Проверка конец

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
+from selenium.webdriver.chrome.options import  Options # для вне визуального запуска
 from selenium import webdriver
 import pytest
 
 @pytest.fixture
 def driver(): # предусловие
-    driver = webdriver.Chrome() #открывает браузер
+    options = Options() # для вне визуального запуска
+    options.add_argument('--headless') # для вне визуального запуска
+    driver = webdriver.Chrome(options=options) #открывает браузер и использует опции при запуске
     driver.maximize_window() #открытие окна на весь экран
     driver.implicitly_wait(3) #ожидание
     yield driver
